aae20480ad580a03ccdac7c7fb52ee6fd89e337e
Vitor de Lima <vitor_cirqueira1@hotmail.com>
2023-02-14 21:48:46 -0300

    feat: add knight dtos

--- api/src/knights/dto/create-knight.dto.ts
+++ api/src/knights/dto/create-knight.dto.ts
@@ -1,1 +1,27 @@
-export class CreateKnightDto {}
+import { IsArray, IsNotEmpty, IsObject, IsOptional, IsString } from "class-validator";
+
+export class CreateKnightDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly nickname: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly birthday: string;
+
+    @IsArray()
+    @IsOptional()
+    readonly weapons: object;
+
+    @IsObject()
+    @IsOptional()
+    readonly attributes: object;
+
+    @IsString()
+    @IsOptional()
+    readonly keyAttribute: string;
+}
