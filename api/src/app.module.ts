4fdf70f53e42d5506eb68d26588b03a79eaf465b
Vitor de Lima <vitor_cirqueira1@hotmail.com>
2023-02-15 08:25:00 -0300

    chore: config env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { KnightsModule } from './knights/knights.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     KnightsModule,
-    MongooseModule.forRoot('mongodb+srv://nestjsdb:nestjsdb@cluster0.sq0r7.mongodb.net/?retryWrites=true&w=majority', {dbName: 'nestjsdb'})
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL, { dbName: process.env.DATABASE_NAME }),
   ],
   controllers: [],
   providers: [],
