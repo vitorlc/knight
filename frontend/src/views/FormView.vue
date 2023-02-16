4b138aa365a1f94ce48cd4f8977cac1198a2f2e2
Vitor de Lima <vitor_cirqueira1@hotmail.com>
2023-02-16 08:37:45 -0300

    feat: finish form and home view

--- frontend/src/views/FormView.vue
+++ frontend/src/views/FormView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Input, InputAddonItem } from "agnostic-vue";
+import { Input, InputAddonItem, Switch, Button } from "agnostic-vue";
 import IconStar from "../components/icons/IconStar.vue";
 import "agnostic-vue/dist/common.min.css";
 import "agnostic-vue/dist/index.css";
@@ -8,148 +8,119 @@ import "agnostic-vue/dist/index.css";
 <template>
   <div class="form">
     <form @submit.prevent="submitForm">
-      <Input
-        v-model="knight.name"
-        label="Nome"
-        type="text"
-        placeholder="Nome do Cavaleiro"
-      /><br />
-      <Input
-        v-model="knight.nickname"
-        label="Nickname"
-        type="email"
-        placeholder="Nick do Cavaleiro"
-      /><br />
-      <Input
-        v-model="knight.birthday"
-        label="Data Nascimento"
-        type="date"
-      /><br />
+      <Input v-model="knight.name" label="Nome" type="text" placeholder="Nome do Cavaleiro" /><br />
+      <Input v-model="knight.nickname" label="Nickname" type="text" placeholder="Nick do Cavaleiro" /><br />
+      <Input v-model="knight.birthday" label="Data Nascimento" type="date" /><br />
+
       <h3>Atributos</h3>
-      <br />
       <section class="flex flex-row flex-wrap">
         <div class="mis6">
-          <Input
-            v-model="attributes.strength"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Força"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('strength')">
-                  {{ knight.keyAttribute == "strength" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.strength" is-inline size="small" has-right-addon label="Força" min="0" type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('strength')">
+                {{ knight.keyAttribute == "strength" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
         <div class="mis6">
-          <Input
-            v-model="attributes.dexterity"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Destreza"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('dexterity')">
-                  {{ knight.keyAttribute == "dexterity" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.dexterity" is-inline size="small" has-right-addon label="Destreza" min="0"
+            type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('dexterity')">
+                {{ knight.keyAttribute == "dexterity" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
         <div class="mis6">
-          <Input
-            v-model="attributes.constitution"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Constituição"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('constitution')">
-                  {{ knight.keyAttribute == "constitution" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.constitution" is-inline size="small" has-right-addon label="Constituição" min="0"
+            type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('constitution')">
+                {{ knight.keyAttribute == "constitution" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
         <div class="mis6">
-          <Input
-            v-model="attributes.intelligence"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Inteligência"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('intelligence')">
-                  {{ knight.keyAttribute == "intelligence" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.intelligence" is-inline size="small" has-right-addon label="Inteligência" min="0"
+            type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('intelligence')">
+                {{ knight.keyAttribute == "intelligence" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
         <div class="mis6">
-          <Input
-            v-model="attributes.wisdom"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Sabedoria"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('wisdom')">
-                  {{ knight.keyAttribute == "wisdom" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.wisdom" is-inline size="small" has-right-addon label="Sabedoria" min="0"
+            type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('wisdom')">
+                {{ knight.keyAttribute == "wisdom" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
         <div class="mis6">
-          <Input
-            v-model="attributes.charisma"
-            is-inline
-            size="small"
-            has-right-addon
-            label="Carisma"
-            min="0"
-            type="number"
-          >
-            <template #addonRight>
-              <InputAddonItem addon-right>
-                <a @click="selectkeyAttribute('charisma')">
-                  {{ knight.keyAttribute == "charisma" ? "🌟" : "⭐" }}
-                </a>
-              </InputAddonItem>
-            </template> </Input
-          ><br />
+          <Input v-model="attributes.charisma" is-inline size="small" has-right-addon label="Carisma" min="0"
+            type="number">
+          <template #addonRight>
+            <InputAddonItem addon-right>
+              <a @click="selectkeyAttribute('charisma')">
+                {{ knight.keyAttribute == "charisma" ? "🌟" : "⭐" }}
+              </a>
+            </InputAddonItem>
+          </template> </Input><br />
         </div>
+      </section>
+      <section>
+        <h3>Armas</h3>
+        <Input v-model="weapon.name" label="Nome" type="text" placeholder="Nome da Arma" /><br />
+        <Input v-model="weapon.mod" label="Mod" type="number" min="0" placeholder="Mod" /><br />
+        <Input v-model="weapon.attr" label="Attr" type="text" placeholder="Attr" /><br />
+        <Switch is-bordered labelPosition="right" v-model="weapon.equipped" label="Equipado?" />
+        <Button @click="addWeapon">Adicionar Arma</Button>
+        <TableData :headers="headersTable" :rows="knight.weapons" />
       </section>
       <Input class="submit" type="submit" value="Cadastrar Cavaleiro" />
     </form>
-  </div>
+</div>
 </template>
 
-<script>
+<script >
+import knightService from "../services/knight.js";
+import TableData from "../components/TableData.vue";
+
+const headersTable = [
+  {
+    label: "Nome Arma",
+    key: "name",
+  },
+  {
+    label: "Mod",
+    key: "mod",
+  },
+  {
+    label: "Attr",
+    key: "attr",
+  },
+  {
+    label: "Equipado",
+    key: "equipped",
+  }
+]
+
 export default {
+  components: {
+    TableData
+  },
   data() {
     return {
       knight: {
@@ -167,14 +138,35 @@ export default {
         wisdom: 0,
         charisma: 0,
       },
+      weapon: {
+        name: "",
+        mod: 0,
+        attr: "",
+        equipped: true
+      },
+
     };
   },
   methods: {
-    submitForm: function () {
-      this.$router.push({ name: "home" });
+    submitForm: async function () {
+      this.knight.attributes = this.attributes
+      try {
+        await knightService.create(this.knight)
+        this.$router.push({ name: "home" });
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    addWeapon: function () {
+      this.knight.weapons.push(this.weapon)
+      this.weapon = {
+        name: "",
+        mod: 0,
+        attr: "",
+        equipped: true
+      }
     },
     selectkeyAttribute: function (attribute) {
-      console.log("🚀 ~ attribute", attribute);
       this.knight.keyAttribute = attribute;
     },
   },
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,56 @@
-<script setup lang="ts">
-import Table from "../components/Table.vue";
-</script>
-
 <template>
   <main>
-    <Table />
-  </main>
+    <TableData :headers="headersTable" :rows="knightsData" />
+</main>
 </template>
+
+<script >
+import TableData from "../components/TableData.vue";
+import knightService from "../services/knight.js";
+
+export default {
+  components: {
+    TableData
+  },
+  data() {
+    return {
+      knightsData: [],
+      headersTable: [
+        {
+          label: "Nome",
+          key: "name",
+        },
+        {
+          label: "Idade",
+          key: "age",
+        },
+        {
+          label: "Armas",
+          key: "weaponsEquipped",
+        },
+        {
+          label: "Atributo",
+          key: "keyAttribute",
+        },
+        {
+          label: "Exp",
+          key: "experience",
+        },
+      ]
+    }
+  },
+  methods: {
+    async getAllKnights() {
+      const { data } = await knightService.findAll()
+      this.knightsData = data.map(elm => {
+        elm.weaponsEquipped = elm.weapons.reduce((acc, cur) => cur.equipped ? acc + 1 : acc, 0)
+        return elm
+      })
+    }
+  },
+  mounted() {
+    this.getAllKnights()
+  }
+}
+</script>
+
