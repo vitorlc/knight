43803002ae8771998a21b04f63dd2b7011d75783
Vitor de Lima <vitor_cirqueira1@hotmail.com>
2023-02-16 08:37:30 -0300

    feat: add table component

---
+++ frontend/src/components/TableData.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="mbe40">
+    <table>
+      <tr>
+        <th v-for="header in props.headers">
+          {{ header.label }}
+        </th>
+      </tr>
+      <tr v-for="row in rows">
+        <td v-for="header in props.headers">
+          {{ row[header.key] == null ? '-' : row[header.key] }}
+        </td>
+      </tr>
+    </table>
+</section>
+</template>
+
+<script setup>
+
+const props = defineProps(['rows', 'headers'])
+
+</script>
+
+<style>
+table {
+  width: 100%;
+}
+
+th {
+  height: 70px;
+  font-weight: bold;
+}
+
+table,
+th,
+td {
+  text-align: center;
+}
+</style>
